1917d4a854f8a85a6c4f1135f6cb2c002de73cf8
alinka955 <78833495+alinka955@users.noreply.github.com>
2022-11-21 21:30:46 -0500

    changes based on feedback
    I still need to fix the courses dropdown menu

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
+let s1 = document.getElementById('subjects');
+let s2 = document.getElementById('courses');
+
 function subjects() {
-    document.getElementById("subjects").classList.toggle("show");
+    s1.classList.toggle("show");
+    addEventListener(onclick, populate(s1, s2)); 
   }
-  function courses() {
-    document.getElementById("courses").classList.toggle("show");
-  }
+
+  //function courses() {
+  //  document.getElementById("courses").classList.toggle("show");
+  //}
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
@@ -21,7 +26,25 @@ function subjects() {
     }
   }
 
+// adjust course names based on the subject selected
+function populate(s1, s2){
+  //s2.innerHTML = "";
+  //console.log(s1.value);
+  if(s1.value == "Accounting"){
+    var optionArray = ["acct100|ACCT 100", "acct200|ACCT 200"];
+  }
+  for(var option in optionArray){ 
+    var pair = optionArray[option].split("|");
+    var newOption = document.createElement("option");
+    newOption.value = pair[0];
+    newOption.innerHTML = pair[1];
+  }
+}
+
 function selectedTutor() {
   let box = document.getElementsByClassName("entry");
   box.className = "entry selected";
 }
+
+
+
