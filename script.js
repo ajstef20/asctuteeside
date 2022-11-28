aa6f842e4691210cff9ed9b33d2b8f40b0eea81f
alinka955 <78833495+alinka955@users.noreply.github.com>
2022-11-28 15:27:05 -0500

    Add files via upload
    new changes

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
+let s1 = document.getElementById('subjects');
+let s2 = document.getElementById('courses');
+let course_listing = document.getElementById("course-listing");
+let item = document.getElementById('acct');
+item.addEventListener("click", populate);
+
+
 function subjects() {
-    document.getElementById("subjects").classList.toggle("show");
+    s1.classList.toggle("show");
+    console.log("ggf");
+    
+    //addEventListener("click", populate(s1, s2)); 
   }
-  function courses() {
-    document.getElementById("courses").classList.toggle("show");
-  }
+
+  //function courses() {
+  //  document.getElementById("courses").classList.toggle("show");
+  //}
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
@@ -21,7 +32,31 @@ function subjects() {
     }
   }
 
+// adjust course names based on the subject selected
+function populate(s1, s2){
+  //s2.innerHTML = "";
+  console.log("ss");
+  console.log(item.textContent);
+  if(item.textContent == "Accounting"){
+    var optionArray = ["acct100|ACCT 100", "acct200|ACCT 200"];
+    
+  }
+  for(var option in optionArray){ 
+    
+    var pair = optionArray[option].split("|");
+    console.log(pair);
+    var newOption = document.createElement("option");
+    newOption.value = pair[0];
+    newOption.innerHTML = pair[1];
+    console.log(newOption);
+    course_listing.appendChild(newOption);
+  }
+}
+
 function selectedTutor() {
   let box = document.getElementsByClassName("entry");
   box.className = "entry selected";
 }
+
+
+
